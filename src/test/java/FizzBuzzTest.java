b1e3cad25c01c22b5c46388e4e092519c315cd03
mirko muller <mirko.muller@heg-arc.ch>
2018-05-07 12:10:32 +0200

    first commit

---
+++ src/test/java/FizzBuzzTest.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class FizzBuzzTest {
+
+
+  @Test
+  public void testHello() {
+    int expected = 0;
+    // Act / Operate
+    int actual = 0;
+    // Assert / Check
+    assertEquals(expected, actual);
+  }
+}
